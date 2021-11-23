f9661b40e61a8187c29aa30ec3664f60c7b6eb90
github username <github email address>
2021-11-23 14:23:30 -0500

    rental image decorators and tests

---
+++ app/components/rental/image.js
@@ -0,0 +1,15 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking'
+import { action } from '@ember/object'
+
+export default class RentalImageComponent extends Component {
+  @tracked isLarge = false;
+
+  @action toggleSize() {
+    this.isLarge = !this.isLarge;
+  }
+  // constructor(...args) {
+  //   super(...args);
+  //   this.isLarge = false;
+  // }
+}
